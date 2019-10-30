0ab2dec612f9a1832c2c62fb38e218a23240b3c5
Ben Rowe <ben.rowe@whispli.com>
2019-10-30 17:44:43 +1100

    Implement basic http response for grpc implemenation

--- src/go-grpc/main.go
+++ src/go-grpc/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main()  {
+import (
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+)
 
+func main() {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+        w.Header().Set("content-type", "application/json")
+        _, err := w.Write([]byte(`["OK"]`))
+        if err != nil {
+            log.Printf("unable to write bytes to response: %v", err)
+        }
+    }).Methods("GET")
+
+
+    r.Use(printRequest)
+
+    log.Printf("serving on :80")
+    if err := http.ListenAndServe(":80", r); err != nil {
+        log.Fatalf("unable to serve request: %v", err)
+    }
+}
+
+func printRequest(next http.Handler) http.Handler {
+    return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+        log.Printf("receiving request: %v", r.URL.Path)
+        next.ServeHTTP(w, r)
+    })
 }
