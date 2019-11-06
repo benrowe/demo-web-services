1eaa98c32979abe13ad7ed12d9679fb7339a2905
Ben Rowe <ben.rowe@whispli.com>
2019-11-06 22:14:12 +1100

    Implement additional orm models

--- src/app/models/models.go
+++ src/app/models/models.go
@@ -1,9 +1,44 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+    "github.com/jinzhu/gorm"
+    "time"
+)
+
+type Gender string
 
 type Employee struct {
     gorm.Model
     FirstName string
     LastName  string
+    BirthDate *time.Time
+    Gender Gender
+    StartDate *time.Time
+    Salaries []Salary
+}
+
+type Department struct {
+    gorm.Model
+
+}
+
+type Salary struct {
+    gorm.Model
+    Salary int
+    FromDate *time.Time
+    ToDate *time.Time
+}
+
+type Manager struct {
+    gorm.Model
+}
+
+type Title struct {
+    gorm.Model
+}
+
+// Migrate the database to match the defined model structure
+func Migrate(db *gorm.DB) {
+    db.AutoMigrate(&Employee{}, &Department{}, &Salary{}, &Manager{}, &Title{})
+
 }
