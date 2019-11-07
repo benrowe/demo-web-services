90303d70595a45a79a15beb2c3e2b6b11b2549ef
Ben Rowe <ben.rowe@whispli.com>
2019-11-08 08:16:21 +1100

    Implement create and list employees in gql

--- src/go-gqlgen/resolvers/resolver.go
+++ src/go-gqlgen/resolvers/resolver.go
@@ -4,7 +4,8 @@ import (
     "context"
     "github.com/benrowe/demo-web-services/src/app"
     "github.com/benrowe/demo-web-services/src/app/models"
-    "log"
+    "github.com/benrowe/demo-web-services/src/go-gqlgen/resolvers/transformations"
+    log "github.com/sirupsen/logrus"
 
     "github.com/benrowe/demo-web-services/src/go-gqlgen/entities"
     "github.com/benrowe/demo-web-services/src/go-gqlgen/gen"
@@ -12,19 +13,36 @@ import (
 
 // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 
-type Resolver struct{
+type Resolver struct {
     App *app.App
 }
 
+type queryResolver struct{ *Resolver }
+type mutationResolver struct{ *Resolver }
+
 func (r *Resolver) Query() gen.QueryResolver {
-	return &queryResolver{r}
+    return &queryResolver{r}
 }
 
-type queryResolver struct{ *Resolver }
+func (r *Resolver) Mutation() gen.MutationResolver {
+    return &mutationResolver{r}
+}
+
+func (m mutationResolver) CreateEmployee(ctx context.Context, input entities.CreateEmployeeInput) (*entities.Employee, error) {
+    employee, err := transformations.ConvertCreateEmployeeInputToEmployeeModel(&input)
+    if err != nil {
+        log.Printf("unable to convert input into employee model: %v", err)
+        return nil, err
+    }
+
+    m.App.DB.Save(&employee)
+
+    return transformations.ModelToGqlEntityEmployee(employee)
+}
 
 func (r *queryResolver) Employee(ctx context.Context, id string) (*entities.Employee, error) {
-	return &entities.Employee{
-	    ID: id,
+    return &entities.Employee{
+        ID: id,
     }, nil
 }
 
@@ -32,13 +50,15 @@ func (r *queryResolver) Employees(ctx context.Context) ([]*entities.Employee, er
     var e []models.Employee
     r.App.DB.Find(&e)
 
-    log.Printf("%+v", e)
+    var ee []*entities.Employee
 
-	employees := []*entities.Employee{
-	    &entities.Employee{
-	        ID: "asdfasdfasdf",
-        },
+    for _, em := range e {
+        eaaa, err := transformations.ModelToGqlEntityEmployee(&em)
+        if err != nil {
+            return nil, err
+        }
+        ee = append(ee, eaaa)
     }
 
-	return employees, nil
+    return ee, nil
 }
