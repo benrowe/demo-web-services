4dec8c62795dd7f47ced53e8700963a5e0d3d1af
Ben Rowe <ben.rowe@whispli.com>
2019-11-11 08:39:49 +1100

    Tidy transformers

--- src/go-gqlgen/resolvers/transformations/employee.go
+++ src/go-gqlgen/resolvers/transformations/employee.go
@@ -3,12 +3,11 @@ package transformations
 import (
     "github.com/benrowe/demo-web-services/src/app/models"
     "github.com/benrowe/demo-web-services/src/go-gqlgen/entities"
-    log "github.com/sirupsen/logrus"
+    "log"
     "time"
 )
 
 func ConvertCreateEmployeeInputToEmployeeModel(i *entities.CreateEmployeeInput) (*models.Employee, error) {
-    // @todo add validation here?
     log.Printf("converting CreateEmployeeInput to Employee model: %+v", i)
     t, err := time.Parse("2006/01/02", i.DateOfBirth)
     if err != nil {
