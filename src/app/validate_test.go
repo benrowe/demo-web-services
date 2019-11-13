6cd036f9939deb1f3bd6202eaba14bba557eeeaa
Ben Rowe <ben.rowe@whispli.com>
2019-11-14 09:12:23 +1100

    Update validation mechanism to use go-playgrounds

--- src/app/validate_test.go
+++ src/app/validate_test.go
@@ -5,24 +5,24 @@ import (
     "testing"
 )
 
-func TestValidate(t *testing.T) {
+func TestValidateVal(t *testing.T) {
     var msg string
     var ok bool
 
     // valid
-    msg, ok = Validate("value", "value", []string{"required", "min:2"})
+    msg, ok = ValidateVal("valuessss", "value", &[]ValidationRule{"required", "min:2"})
 
     assert.True(t, ok)
     assert.Equal(t, "", msg)
 
-    // first rule invalid
-    msg, ok = Validate("", "value", []string{"required", "min:2"})
+    //first rule invalid
+    msg, ok = ValidateVal("", "value", &[]ValidationRule{"required", "min:2"})
 
     assert.False(t, ok)
     assert.Equal(t, "The value field is required", msg)
 
     // second rule invalid
-    msg, ok = Validate("", "value", []string{"required", "min:2"})
+    msg, ok = ValidateVal("", "value", &[]ValidationRule{"required", "min:2"})
 
     assert.False(t, ok)
     assert.Equal(t, "The value field is required", msg)
