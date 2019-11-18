bac97da020894616cd6038bf224452dd2206036b
Ben Rowe <ben.rowe@whispli.com>
2019-11-19 10:13:39 +1100

    Fix validation on gqlgen

--- src/app/validate.go
+++ src/app/validate.go
@@ -1,18 +1,64 @@
 package app
 
 import (
-    "fmt"
+    "github.com/go-playground/locales"
+    locale_en "github.com/go-playground/locales/en"
+    ut "github.com/go-playground/universal-translator"
     "github.com/go-playground/validator/v10"
+    en_translations "github.com/go-playground/validator/v10/translations/en"
     "log"
     "strings"
 )
+
+var (
+    validate *validator.Validate
+    trans    ut.Translator
+)
+
+// get validator instance
+func get() *validator.Validate {
+    if validate == nil {
+        validate = newInstance("en")
+    }
+    return validate
+}
+
+// newInstance validator instance
+func newInstance(l string) *validator.Validate {
+    availLocales := []locales.Translator{locale_en.New()}
+    uni := ut.New(availLocales[0], availLocales...)
+    t, found := uni.GetTranslator("en")
+    if found == false {
+        log.Printf("unable to get translator %s", l)
+    }
+    trans = t
+
+    v := validator.New()
+    err := en_translations.RegisterDefaultTranslations(v, trans)
+    if err != nil {
+        log.Printf("crap %v", err)
+    }
+    err = v.RegisterTranslation("required", t, func(ut ut.Translator) error {
+        return ut.Add("required", "The {0} field is required", true)
+    }, func(ut ut.Translator, fe validator.FieldError) string {
+        t, _ := ut.T("required", fe.Field())
+
+        return t
+    })
+
+    if err != nil {
+        log.Printf("unable to register translations for validator: %v", err)
+    }
+
+    return v
+}
 
 // ValidationRule
 type ValidationRule string
 
 // forPlayground converts the rule into a format for consumption by go-playground's validator
 func (r ValidationRule) forPlayground() string {
-    return strings.Replace(string(r), ":", "=", 0)
+    return strings.Replace(string(r), ":", "=", -1)
 }
 
 // ValidateVal the value against the rules
@@ -20,7 +66,7 @@ func ValidateVal(val interface{}, name string, rules *[]ValidationRule) (msg str
     msg = ""
     ok = true
 
-    validate := validator.New()
+    validate := get()
 
     s := stringifyRulesForPlayground(rules)
     if s == "" {
@@ -30,7 +76,13 @@ func ValidateVal(val interface{}, name string, rules *[]ValidationRule) (msg str
     err := validate.Var(val, s)
     if err != nil {
         ok = false
-        msg = fmt.Sprintf("The %v field is %s", name, err.Error())
+        errs := err.(validator.ValidationErrors)
+        for _, e := range errs {
+            // can translate each error one at a time.
+            msg = strings.Replace(e.Translate(trans), "  ", " "+name+" ", 1)
+        }
+
+        //msg = errs.Error()
     }
     return
 }
@@ -39,10 +91,11 @@ func ValidateVal(val interface{}, name string, rules *[]ValidationRule) (msg str
 // playgrounds validator
 func stringifyRulesForPlayground(r *[]ValidationRule) string {
     var s []string
-    for i, rr := range *r {
+    //s := []string{}
+    for _, rr := range *r {
         rule := rr.forPlayground()
         if rule != "" {
-            s[i] = rule
+            s = append(s, rule)
         }
     }
     return strings.Join(s, ",")
