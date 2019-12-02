6980ecc796c4f9e274ce284cef043d3418a83aae
Ben Rowe <ben.rowe@whispli.com>
2019-12-03 07:16:07 +1100

    Stub basic functionaltiy for employee entity

--- src/go-gqlgen/entities/employee.go
+++ src/go-gqlgen/entities/employee.go
@@ -1,20 +1,45 @@
 package entities
 
+import "time"
+
 // Employee contains data about a individual
 type Employee struct {
-	ID           string        `json:"id"`
-	FirstName    string        `json:"firstName"`
-	LastName     string        `json:"lastName"`
-	DateOfBirth  string        `json:"dateOfBirth"`
-	Age          int           `json:"age"`
-	Gender       Gender        `json:"gender"`
-	Remuneration *Remuneration `json:"remuneration"`
-	Department   *Department   `json:"department"`
+	ID          string `json:"id"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	DateOfBirth string `json:"dateOfBirth"`
+	Age         int    `json:"age"`
+	Gender      Gender `json:"gender"`
 }
 
 func (e *Employee) CurrentRole() *AssignedRole {
 	// todo implement
-	ar := AssignedRole{}
+	ar := AssignedRole{
+		Role:      &Role{},
+		StartDate: time.Now().Format("2006/01/02"),
+	}
 
 	return &ar
+}
+
+func (e *Employee) Remuneration() *Remuneration {
+	// todo
+	r := Remuneration{
+		Amount: 5,
+	}
+
+	return &r
+}
+
+func (e *Employee) Department() *Department {
+	// todo
+	d := Department{
+		Name: "sdf",
+		Manager: &Employee{
+			FirstName: "Harry",
+			LastName:  "Tild",
+		},
+	}
+
+	return &d
 }
