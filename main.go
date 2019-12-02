a63640e6673f3e5d78e193e7f3eadc9e87dfa523
Ben Rowe <ben.rowe@whispli.com>
2019-12-03 07:20:05 +1100

    Fix formatting

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 type service struct {
-    container string
-    port string
+	container string
+	port      string
 }
 
 func (s service) url() string {
-    return fmt.Sprintf("http://%v:%v", "localhost", s.port)
+	return fmt.Sprintf("http://%v:%v", "localhost", s.port)
 }
 
 func main() {
 
-    services := []*service{
-        &service{container:"go-grpc", port:"802"},
-        &service{container:"go-gqlgen", port: "801"},
-        &service{container:"go-swagger", port:"803"},
-    }
+	services := []*service{
+		&service{container: "go-grpc", port: "802"},
+		&service{container: "go-gqlgen", port: "801"},
+		&service{container: "go-swagger", port: "803"},
+	}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("content-type", "text/html")
-        // generate a list of implementations
-        w.Write([]byte("<ul>"))
-        for _, s := range services {
-            w.Write([]byte("<li><a href=\""+s.url()+"\">"+s.container+"</a></li>"))
-        }
-        w.Write([]byte("</ul>"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "text/html")
+		// generate a list of implementations
+		w.Write([]byte("<ul>"))
+		for _, s := range services {
+			w.Write([]byte("<li><a href=\"" + s.url() + "\">" + s.container + "</a></li>"))
+		}
+		w.Write([]byte("</ul>"))
 
-    })
-    http.ListenAndServe(":80", nil)
+	})
+	http.ListenAndServe(":80", nil)
 }
