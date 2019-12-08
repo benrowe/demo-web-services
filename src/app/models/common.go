afd8eb747dfadecec9b462ff65c0c8a4dc3f90d1
Ben Rowe <ben.rowe@whispli.com>
2019-12-09 07:52:00 +1100

    Add logging to db migration

--- src/app/models/common.go
+++ src/app/models/common.go
@@ -1,8 +1,12 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"log"
+)
 
 // Migrate the database to match the defined model structure
 func Migrate(db *gorm.DB) {
+	log.Println("migrating db")
 	db.AutoMigrate(&Employee{}, &Department{}, &Salary{}, &Manager{}, &Title{})
 }
